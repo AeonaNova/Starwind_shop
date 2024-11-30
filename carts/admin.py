34a267f1e9847db98b74f806ae34effc28221145
Res <glowingghostred@gmail.com>
2024-11-30 14:53:04 +0300

    add picture to cart, add scrolling bar to user-cart

---
+++ carts/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Cart
+
+class CartView(admin.TabularInline):
+    model = Cart
+    fieds = "product", "quantity", "created_timestamp"
+    search_fields = "product", "quantity", "created_timestamp"
+    readonly_fields = ("creation_timestamp",)
+    extra = 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user_display", "product_display", "quantity", "creation_timestamp",]
+    list_filter = ["creation_timestamp", "user", "product__name", ]
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Анонимный пользователь"
+
+    def product_display(self, obj):
+        return str(obj.product.name)
+
+    # user_display and product_display alter name of columns in admin panel
+    user_display.short_description = "Пользователь"
+    product_display.short_description = "Товар"
---
+++ carts/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+import goods
+import users
+from goods.models import Goods
+
+
+# Create your models here.
+class CartQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(to='users.User', null=True, blank=True, on_delete=models.CASCADE, verbose_name='Пользователь')
+    product = models.ForeignKey(to='goods.Goods', on_delete=models.CASCADE, verbose_name='Товар')
+    creation_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+        ordering = ("id",)
+
+    objects = CartQueryset().as_manager()
+
+    def products_price(self):
+        return round(self.product.final_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f'Корзина {self.user} | Товар {self.product.name} | Количество {self.quantity}'
